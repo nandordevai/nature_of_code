5091f13dd4c752c0038e54b70e5b3fa85b3f9740
Nandor Devai <mail@devainandor.com>
2018-07-02 23:01:44 +0200

    splatter

---
+++ src/nature_of_code/splatter.clj
@@ -0,0 +1,25 @@
+(ns splatter
+  (:use quil.core))
+
+(defn setup []
+  (background 255)
+  (no-stroke))
+
+(defn draw []
+  (no-loop)
+  (def r 20)
+  (def sd 55)
+  (with-translation [(/ (width) 2) (/ (height) 2)]
+    (loop [i 0]
+      (when (< i 200)
+        (def x (int (* sd (random-gaussian))))
+        (def y (int (* sd (random-gaussian))))
+        (fill 120 (+ 100 x) (+ 170 y) 200)
+        (ellipse x y r r)
+        (recur (inc i))))))
+
+(defsketch s
+  :size [800 600]
+  :draw draw
+  :settings #(.pixelDensity (quil.applet/current-applet) (.displayDensity (quil.applet/current-applet)))
+  :setup setup)
